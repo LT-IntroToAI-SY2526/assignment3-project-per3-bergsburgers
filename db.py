3f37fe3dd4b070d0c714a9ea708db06f281f7ad1
Dillon Shannon <dshannon8@cps.edu>
2025-10-17 09:57:44 -0500

    completed database

---
+++ db.py
@@ -0,0 +1,65 @@
+from typing import List, Tuple
+
+player_db: List[Tuple[str, str, int, List[str]]] = [ 
+    (
+        "Tom Brady",
+        "New England Patriots and Tampa Bay Bucaneers",
+        2000,
+        2022,
+        [
+            "7 Super Bowls won", # total super bowl wins
+            "737 total career touchdowns", # total career touchdowns
+            "102,614 total yards", # total yards over entire career
+            "3 MVP's and 5 Super Bowl MVP's" # total MVP's
+        ]
+    )
+    (
+        "Jim Brown",
+        "Cleveland Browns",
+        1957,
+        1965,
+        [
+            "1 Championship won(Pre Super Bowl era)",
+            "126 total career touchdowns",
+            "15,459 total yards",
+            "3 MVP's",
+        ]
+        
+    )
+    (
+        "Jerry Rice",
+        "San Francisco 49ers, Oakland Raiders and Seattle Seahawks",
+        1985,
+        2004,
+        [
+            "3 Super Bowls won",
+            "208 total career touchdowns",
+            "22,895 total yards",
+            "1 Super Bowl MVP",
+        ]
+    )
+    (
+        "Randy Moss",
+        "Minnesota Vikings, Oakland Raiders, New England Patriots, Tennessee Titans and San Fransisco 49ers",
+        1998,
+        2012,
+        [
+            "0 Super Bowls won",
+            "157 total career touchdowns",
+            "15,292 total yards",
+            "1 Pro Bowl MVP",
+        ]
+    )
+    (
+        "Tony Gonzalez",
+        "Kansas City Chiefs and Atlanta Falcons",
+        1997,
+        2013,
+        [
+            "0 Super Bowls Won",
+            "111 total career touchdowns",
+            "15,141 total yards",
+            "One team MVP for the Chiefs",
+        ]
+    )
+]
